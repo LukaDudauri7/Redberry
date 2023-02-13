8ce823b6154c79fa106a417ca69f64c262a625d8
SMARTSOFT\L.Dudauri <l.dudauri@smartsoft.ge>
2023-02-13 19:22:12 +0400

    111

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes , HashRouter } from "react-router-dom";
 import Main from "./Components/Main/Main";
 import Scale from './Scale';
 import AddResume from "./Components/AddResume/AddResume";
@@ -9,13 +9,13 @@ function App() {
   return (
     <div className="App">
       <Scale>
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<AddResume />} />
             <Route path="/Main" element={<Main />} />
             {/* <Route path="/Experience" element={<Experience />} /> */}
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </Scale>
     </div>
   );
